ac70837a13c9e9314ec5688b3f569e0aafe1e08c
oNqNu <onanukintaro@gmail.com>
2022-03-01 21:18:11 +0900

    create_app 卒業

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
-import os
 from flask import Flask
 from flask_cors import CORS
 
-def create_app(test_config=None):
-    
-    app = Flask(__name__)
-    CORS(app)
-    app.config['UPLOAD_FOLDER'] = './uploads'
 
-    from routes import processing
-    app.register_blueprint(processing.bp)
+app = Flask(__name__)
+CORS(app)
+app.config['UPLOAD_FOLDER'] = './uploads'
 
+from routes import processing
+app.register_blueprint(processing.bp)
 
-    return app
 
 if __name__ == "__main__":
-    app = create_app()
     app.run()
-
