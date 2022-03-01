aad4b65183ad5aa17025fd2023befacf1f02b515
oNqNu <onanukintaro@gmail.com>
2022-03-01 21:44:32 +0900

    dd

--- app.py
+++ app.py
@@ -2,18 +2,18 @@ import os
 from flask import Flask
 from flask_cors import CORS
 
-def create_app(test_config=None):
-    
-    app = Flask(__name__)
-    CORS(app)
-    app.config['UPLOAD_FOLDER'] = './uploads'
+  
+app = Flask(__name__)
+CORS(app)
+app.config['UPLOAD_FOLDER'] = './uploads'
 
-    from routes import processing
-    app.register_blueprint(processing.bp)
+from routes import processing
+app.register_blueprint(processing.bp)
 
+app.run()
 
-    return app
+#     return app
 
 # if __name__ == "__main__":
-app = create_app()
-    # app.run()
+#     app = create_app()
+#     app.run()
