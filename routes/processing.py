6ece78f5d6612415d1b95aa5eab6f2e869f2fced
oNqNu <onanukintaro@gmail.com>
2022-03-01 16:35:46 +0900

    平滑化とエッジ検出api

--- routes/processing.py
+++ routes/processing.py
@@ -1,6 +1,7 @@
 import shutil
 import os
 import cv2
+import numpy as np
 import base64
 
 
@@ -25,12 +26,8 @@ def clear_uploads():
     shutil.rmtree('uploads')
     os.mkdir('uploads')
 
-@bp.route('/hello')
-def hello():
-    return 'Hello, oNqNu!'
-
-@bp.route("/test", methods=['POST'])
-def test():
+@bp.route("/grayscale", methods=['POST'])
+def grayscale():
     if 'file0' not in request.files:
             return 'ファイルがありません．'
 
@@ -55,5 +52,60 @@ def test():
             img_str = cv_to_base64(im_gray)
             return img_str
     
+    return 'qqq'
 
+@bp.route("/smoothing", methods=['POST'])
+def smoothing():
+    if 'file0' not in request.files:
+            return 'ファイルがありません．'
+
+    img = request.files.get('file0')
+    print(type(img))
+
+
+    if img.filename == '':
+            return 'ファイル名が確認できない．'
+
+    if img and allwed_file(img.filename):
+            # 危険な文字を削除（サニタイズ処理）
+            filename = secure_filename(img.filename)
+            # ファイルの保存
+            img.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+            # アップロード後のページに転送
+            img = cv2.imread('uploads/' + filename)
+            print(img.shape)
+            im_smooth = cv2.blur(img,(11,11))
+            cv2.imwrite('uploads/gray.jpg', im_smooth)
+            clear_uploads()
+            img_str = cv_to_base64(im_smooth)
+            return img_str
+    
+    return 'qqq'
+
+@bp.route("/edge_detection", methods=['POST'])
+def edge_detection():
+    if 'file0' not in request.files:
+            return 'ファイルがありません．'
+
+    img = request.files.get('file0')
+    print(type(img))
+
+
+    if img.filename == '':
+            return 'ファイル名が確認できない．'
+
+    if img and allwed_file(img.filename):
+            # 危険な文字を削除（サニタイズ処理）
+            filename = secure_filename(img.filename)
+            # ファイルの保存
+            img.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+            # アップロード後のページに転送
+            img = cv2.imread('uploads/' + filename)
+            print(img.shape)
+            im_edge = cv2.Canny(img,180,300)
+            cv2.imwrite('uploads/gray.jpg', im_edge)
+            clear_uploads()
+            img_str = cv_to_base64(im_edge)
+            return img_str
+    
     return 'qqq'
