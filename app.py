97105d0ff6272297f336bf0d3fddf4945b08c0f4
oNqNu <onanukintaro@gmail.com>
2022-03-02 11:06:03 +0900

    test

--- app.py
+++ app.py
@@ -7,9 +7,12 @@ app = Flask(__name__)
 CORS(app)
 app.config['UPLOAD_FOLDER'] = './uploads'
 
-from routes import processing
-app.register_blueprint(processing.bp)
+# from routes import processing
+# app.register_blueprint(processing.bp)
 
+@app.route('/')
+def index():
+    return 'hello, world'
 
 
 if __name__ == "__main__":
